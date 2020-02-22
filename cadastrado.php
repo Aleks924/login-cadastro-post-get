34731f80e7744668fc4b16aad31fd025d44c40cc
Aleks <aleks.v924@gamil.com>
2020-02-21 22:01:19 -0300

    Criação de aruqivos de cadatro

---
+++ cadastrado.php
@@ -0,0 +1,7 @@
+<meta charset="utf-8"/>
+<?php
+    $nomeDoUsuario = $_POST["nome"];
+    $cpfDoUsuario = $_POST["cpf"];
+    $idadeDoUsuario = $_POST["idade"];
+        echo "$nomeDoUsuario, voçê foi cadastrado com suceso";
+?>
